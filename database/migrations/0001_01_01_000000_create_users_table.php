84124a00cfaaf8490be4f065cfd2a883b8f9e9e7
WahabSyahranie <wahabschools@gmail.com>
2025-04-26 17:05:18 +0800

    second commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
-
+        
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
