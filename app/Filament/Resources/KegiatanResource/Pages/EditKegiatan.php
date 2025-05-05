9f43a7df762edc4f7659f18ed09473794a2e4123
WahabSyahranie <wahabschools@gmail.com>
2025-05-05 20:11:02 +0800

    save

--- app/Filament/Resources/KegiatanResource/Pages/EditKegiatan.php
+++ app/Filament/Resources/KegiatanResource/Pages/EditKegiatan.php
@@ -2,18 +2,27 @@
 
 namespace App\Filament\Resources\KegiatanResource\Pages;
 
-use App\Filament\Resources\KegiatanResource;
 use Filament\Actions;
+use App\Models\Kegiatan;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\KegiatanResource;
+use App\Filament\Widgets\KegiatanResourceAbsenWidget;
 
 class EditKegiatan extends EditRecord
 {
     protected static string $resource = KegiatanResource::class;
+    
+    // protected function getFooterWidgets(): array
+    // {
+    //     return [
+    //         KegiatanResourceAbsenWidget::make()
+    //     ];
+    // }
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
--- app/Filament/Resources/KegiatanResource/Pages/ListKegiatans.php
+++ app/Filament/Resources/KegiatanResource/Pages/ListKegiatans.php
@@ -2,14 +2,16 @@
 
 namespace App\Filament\Resources\KegiatanResource\Pages;
 
-use App\Filament\Resources\KegiatanResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\KegiatanResource;
+use App\Filament\Widgets\StatsOverviewWidget;
 
 class ListKegiatans extends ListRecords
 {
     protected static string $resource = KegiatanResource::class;
     protected static ?string $title = 'Daftar Kegiatan';
+
     
     // protected function getHeaderActions(): array
     // {
