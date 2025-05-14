38cabe835f05086179601ee7b75f0d325900f63a
WahabSyahranie <wahabschools@gmail.com>
2025-05-14 15:38:07 +0800

    edit create pengurus, tidak duplikat

--- app/Filament/Resources/PengurusResource/Pages/CreatePengurus.php
+++ app/Filament/Resources/PengurusResource/Pages/CreatePengurus.php
@@ -2,15 +2,35 @@
 
 namespace App\Filament\Resources\PengurusResource\Pages;
 
+use App\Models\User;
 use Filament\Actions;
-use Filament\Notifications\Notification;
-use Filament\Resources\Pages\CreateRecord;
-use App\Filament\Resources\PengurusResource;
+use App\Models\Pengurus;
 use App\HandlesDepartemen;
 use App\Models\DetailPengurus;
 use Filament\Support\Exceptions\Halt;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\PengurusResource;
+use Illuminate\Validation\ValidationException;
 
 class CreatePengurus extends CreateRecord
 {
     protected static string $resource = PengurusResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $exists = Pengurus::where('user_id', $data['user_id'])
+            ->exists();
+
+        if ($exists) {
+            Notification::make()
+                ->title('Data Pengurus sudah ada!')
+                ->danger()
+                ->send();
+            throw ValidationException::withMessages([
+                'user_id' => ['Data pengurus ini sudah terdaftar.'],
+            ]);
+        }
+        return $data;
+    }
 }
