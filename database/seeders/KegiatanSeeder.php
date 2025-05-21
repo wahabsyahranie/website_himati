5a8320d66061ec88f11e7b0285f1df121408e480
WahabSyahranie <wahabschools@gmail.com>
2025-05-22 00:50:56 +0800

    up up

--- database/seeders/KegiatanSeeder.php
+++ database/seeders/KegiatanSeeder.php
@@ -17,7 +17,7 @@ class KegiatanSeeder extends Seeder
         DB::table('kegiatans')->insert([
             [
                 'nama' => 'Rapat Umum Awal Tahun',
-                'tanggal_pelaksana' => '2025-01-15 09:00:00',
+                'tanggal_pelaksana' => '2025-05-24 09:00:00',
                 'status' => 0,
                 'jenis_kegiatan' => 'rapat umum',
                 'tempat_pelaksanaan' => 'Aula Teknologi Informasi',
@@ -26,7 +26,7 @@ class KegiatanSeeder extends Seeder
             ],
             [
                 'nama' => 'Rapat Koordinasi Panitia SERASI',
-                'tanggal_pelaksana' => '2025-02-10 13:30:00',
+                'tanggal_pelaksana' => '2025-05-27 13:30:00',
                 'status' => 0,
                 'jenis_kegiatan' => 'rapat panitia',
                 'tempat_pelaksanaan' => 'Aula Teknologi Informasi',
@@ -35,7 +35,7 @@ class KegiatanSeeder extends Seeder
             ],
             [
                 'nama' => 'Pelaksanaan SERASI 2025',
-                'tanggal_pelaksana' => '2025-03-20 08:00:00',
+                'tanggal_pelaksana' => '2025-05-17 08:00:00',
                 'status' => 0,
                 'jenis_kegiatan' => 'proker primer',
                 'tempat_pelaksanaan' => 'Aula Teknologi Informasi',
@@ -44,7 +44,7 @@ class KegiatanSeeder extends Seeder
             ],
             [
                 'nama' => 'Pembuatan Konten Sosial Media',
-                'tanggal_pelaksana' => '2025-02-25 15:00:00',
+                'tanggal_pelaksana' => '2025-05-19 15:00:00',
                 'status' => 0,
                 'jenis_kegiatan' => 'proker sekunder',
                 'tempat_pelaksanaan' => 'Aula Teknologi Informasi',
@@ -53,7 +53,7 @@ class KegiatanSeeder extends Seeder
             ],
             [
                 'nama' => 'Rapat Evaluasi Tengah Periode',
-                'tanggal_pelaksana' => '2025-04-05 10:00:00',
+                'tanggal_pelaksana' => '2025-05-28 10:00:00',
                 'status' => 0,
                 'jenis_kegiatan' => 'rapat umum',
                 'tempat_pelaksanaan' => 'Aula Teknologi Informasi',
